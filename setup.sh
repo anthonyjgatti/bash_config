4ce2fd2d40a8fc06110b9774714979f9bf02f3d9
anthonyjgatti <anthony.j.gatti@gmail.com>
2018-02-05 11:30:46 -0500

    Updates to setup script.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 
 sudo yum install vim -y
 sudo cp ~/bash_config/.vim/colors/monokai.vim /usr/share/vim/vim74/colors
+cp -r ~/bash_config/.bash_profile ~/.bash_profile
 echo 'alias vim="vim -u ~/bash_config/"' >> ~/.bash_profile
+source ~/.bash_profile
 
