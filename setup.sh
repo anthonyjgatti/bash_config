6138070d5cbdf31208f1d04990f14fc0626003eb
anthonyjgatti <anthony.j.gatti@gmail.com>
2018-02-05 13:24:10 -0500

    Update setup.sh to remove install commands.

--- setup.sh
+++ setup.sh
@@ -4,9 +4,8 @@
 MOUNT_DIR=/code
 
 # Copy configuration to $HOME directory.
-sudo yum install vim -y
 sudo cp $MOUNT_DIR/bash_config/.vim/colors/monokai.vim /usr/share/vim/vim74/colors
+cp -r $MOUNT_DIR/bash_config/.vimrc ~/.vimrc
 cp -r $MOUNT_DIR/bash_config/.bash_profile ~/.bash_profile
-echo 'alias vim="vim -u ~/bash_config/.vimrc"' >> ~/.bash_profile
 source ~/.bash_profile
 
